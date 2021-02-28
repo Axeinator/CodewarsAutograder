6bef669897b0d4ccec9f41adf1a08699e9496bd1
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-02-28 18:51:32 +0000

    Bug fixes and better file names in tmp/

--- c++runner.py
+++ c++runner.py
@@ -1,21 +1,24 @@
 import os
-from formatters import zeroFill
+from formatters import zeroFill, formatInFile
 
 # looks like C++ didn't have the
 # same formatting problems as python
 # with newlines and carriage returns
-# so this does not use the 
-# formatInFile function
+# but still am using formatInFile to 
+# prevent errors
+
 
 def runCpp(problem):
   problem = zeroFill(problem)
+  formatInFile(problem)
   os.system(f"g++ -o tmp/prob{problem}.out prob{problem}.cpp")
   for i in range(1, 4):
-        os.system(f"./tmp/prob{problem}.out < tmp/tmp{i}.txt > tmp/your-prob{problem}-{i}-out.txt")
+        os.system(f"./tmp/prob{problem}.out < tmp/tmp{i}-in.txt > tmp/your-prob{problem}-{i}-out.txt")
         code = os.system(f'diff -w -B -I "\n" tmp/your-prob{problem}-{i}-out.txt student_datasets/prob{problem}-{i}-out.txt')
         if code == 0: # files identical
           print("Suceeded!")
         else:
           print("Failed :(")
+  os.system(f"rm tmp/prob{problem}.out")
 
 runCpp(3)
